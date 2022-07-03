d859f99ec61b350f8185791dcf2c86b563f2570e
bairdob <krumane@mail.ru>
2022-07-03 18:48:56 +0300

    add /api/calculate_orthodrome_line

--- app.py
+++ app.py
@@ -1,25 +1,17 @@
 from flask import Flask
 from flask import redirect, render_template, url_for, request
 
-from geomet import wkt
-
 from wtforms.form import Form
-from wtforms.fields import StringField, SelectField, SubmitField
+from wtforms.fields import StringField, SelectField, SubmitField, TextAreaField
 from wtforms.validators import DataRequired, Regexp
 
-import re
+from shapely import wkt
+from pyproj import Geod
 
-import json
+import re
+import requests
 
 app = Flask(__name__)
-
-
-def get_geojson_from_wkt(wkt_string):
-    return wkt.loads(wkt_string)
-
-
-# ls = 'POINT(37.6245 55.123123)'
-# print(get_geojson_from_wkt(ls))
 
 PATTERN_POINT = '^POINT\([0-9]*.[0-9]+ [0-9]*.[0-9]+\)'
 
@@ -35,6 +27,7 @@ class DataForm(Form):
     input_count = StringField(default='10',
                               validators=[DataRequired(),
                                           Regexp('^[0-9]+$')])
+    output_linestring = TextAreaField('LINESTRING(0.0)')
     submit = SubmitField("Submit")
 
 
@@ -56,10 +49,39 @@ def orthodromy():
     orthodromyForm.input_count.description = 'Количество рассчитываемых точек, по-умолчанию 10'
 
     if request.method == 'POST' and orthodromyForm.validate():
-        print('done')
-        return render_template('orthodromy.html')
+        linestring = url_for('calculate_orthodrome_line',
+                             point1=orthodromyForm.input_point1.data,
+                             point2=orthodromyForm.input_point2.data,
+                             cs=orthodromyForm.input_coordinate_system.data,
+                             count=orthodromyForm.input_count.data)
+        my_url = "http://127.0.0.1:5000" + linestring
+
+        orthodromyForm.output_linestring.data = requests.post(url=my_url).text
+
+        return render_template('orthodromy.html', orthodromyForm=orthodromyForm)
 
     return render_template('orthodromy.html', orthodromyForm=orthodromyForm)
+
+
+@app.route('/api/calculate_orthodrome_line', methods=['GET', 'POST'])
+def calculate_orthodrome_line():
+    point1 = wkt.loads(request.args.get('point1'))  # return Point(x,y)
+    point2 = wkt.loads(request.args.get('point2'))
+    cs = request.args.get('cs')
+    count = int(request.args.get('count'))
+
+    if cs == 'СК-42':
+        geoid = Geod(ellps="WGS84")
+        extra_points = geoid.npts(point1.x, point1.y, point2.x, point2.y,
+                                  count)
+
+        points = str(extra_points).strip('[]').replace(',', '').replace(
+            '(', '').replace(')', ',')
+        linestring = 'LINESTRING(' + points.strip(',') + ')'
+
+        return linestring, 200
+
+    return 'Wrong coordinate system', 400
 
 
 if __name__ == '__main__':
