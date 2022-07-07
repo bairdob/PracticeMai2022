20944e2e7d5ed3ecb01455efaa6b565089a38cf5
Sergey Efimov <efimovsn@gmail.com>
2022-07-07 20:01:40 +0300

    #1 убрал копипасту

--- static/demo.js
+++ static/demo.js
@@ -17,61 +17,24 @@ $(function() {
 
 })
 
-function makePoint1() {
-    var marker1;
-    map.on('click', function(event){
+let markers = {};
+function makePoint(inputId, markerId) {
 
-        if (marker1 == undefined) {    
-            marker1 = L.marker(event.latlng);
-
-            marker1.addTo(map);
-
-            text1 = "POINT("+event.latlng.lng.toString()+" "+event.latlng.lat.toString()+")"
-            marker1.bindPopup(text1).openPopup();
-            document.getElementById('input_point1').value = text1;
-
-        }       
-        else {
-            map.removeLayer(marker1);
-            // map.removeLayer(markers);
-            marker1 = L.marker(event.latlng)
-            point1 = marker1;
-            marker1.addTo(map);
-            map.invalidateSize()
-            text1 = "POINT("+event.latlng.lng.toString()+" "+event.latlng.lat.toString()+")"
-            marker1.bindPopup(text1).openPopup();
-            document.getElementById('input_point1').value = text1;
-
+    let wkt;
+    map.on('click', function(event) {
+        if (markers[markerId]) {
+            map.removeLayer(markers[markerId]);
+            map.invalidateSize();
         }
-                        
-    });
- 
-}
-
-function makePoint2() {
-    var marker;
-
-    map.on('click', function(event){
-
-        if (marker == undefined) {    
-            marker = L.marker(event.latlng);
-            marker.addTo(map);
-            text2 = "POINT("+event.latlng.lng.toString()+" "+event.latlng.lat.toString()+")"
-            marker.bindPopup(text2).openPopup();
-            document.getElementById('input_point2').value = text;
-        }       
-        else {
-            map.removeLayer(marker);
-            marker = L.marker(event.latlng);
-            marker.addTo(map);
-            text2 = "POINT("+event.latlng.lng.toString()+" "+event.latlng.lat.toString()+")"
-            marker.bindPopup(text2).openPopup();
-            document.getElementById('input_point2').value = text2;
-        }
-                        
+        markers[markerId] = L.marker(event.latlng);
+        markers[markerId].addTo(map);
+        wkt = "POINT("+event.latlng.lng.toString()+" "+event.latlng.lat.toString()+")"
+        console.log(markers, wkt)
+        document.getElementById(inputId).value = wkt;
     });
 
 }
+
 
 
 function test(form){
