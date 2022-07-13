d2d275cb95e968f6e010b34eb0ccf5ac714c0ac6
bairdob <krumane@mail.ru>
2022-07-13 09:07:27 +0300

    add custom usecase for elevation service

--- static/elevation.js
+++ static/elevation.js
@@ -16,7 +16,7 @@ var imageOverlay = L.imageOverlay(imageUrl, latLngBounds, {
     interactive: true
 }).addTo(map);
 
-L.rectangle(latLngBounds).addTo(map);
+L.rectangle(latLngBounds, {opacity: 0.5}).addTo(map);
 map.fitBounds(latLngBounds)
 
 // var2__________
@@ -38,19 +38,31 @@ map.fitBounds(latLngBounds)
 
 // map.setMaxBounds(bounds);
 
+
+function clean_map(){
+
+    if (marker) {
+        map.removeLayer(marker);
+    }
+
+    // if (polyline) {
+    //     map.removeLayer(polyline);
+    // }
+
+}
+
+
 let marker;
 map.on('click', function (event) {
-		if (marker) {
-        map.removeLayer(marker);
-     }
-     marker = L.marker(event.latlng);
-     marker.addTo(map);
-     wkt = "POINT("+event.latlng.lng.toString()+" "+event.latlng.lat.toString()+")"
-     marker.bindPopup(wkt).openPopup();
+    clean_map();
+    marker = L.marker(event.latlng);
+    marker.addTo(map);
+    wkt = "POINT("+event.latlng.lng.toString()+" "+event.latlng.lat.toString()+")"
+    marker.bindPopup(wkt).openPopup();
  })
 
 
-function calculate_elevation(){
+function calculate_elevation_select(){
     let sel = document.getElementById('select_wkt');
     let wkt = sel.options[sel.selectedIndex].text;
     let url = 'http://' + window.location.host + '/api/elevation?wkt='+ wkt;
@@ -67,6 +79,11 @@ function calculate_elevation(){
     
 }
 
+document.getElementById("select_wkt").addEventListener("change", (ev) => {
+    output_linestring.innerHTML = '';
+});
+
+
 function check_image() {
 
     var chbox;
@@ -80,4 +97,86 @@ function check_image() {
 
 }
 
+
+document.getElementById("select_format").addEventListener("change", (ev) => {
+    var buttonId = document.getElementById("select_format").value;
+    var button = document.querySelector('[button="' + buttonId + '"]');
+    var buttons = document.querySelectorAll(".hiden_button").forEach(el => {
+        el.style.display = 'none';
+        clean_map();
+    });
+    button.style.display = 'block';
+    output_linestring.innerHTML = '';
+});
+
+
+function makePoint(inputId) {
+
+    let wkt;
+    map.off('click');
+    map.on('click', function(event) {
+        output_linestring.innerHTML = '';        
+        clean_map();
+
+        marker = L.marker(event.latlng);
+        marker.addTo(map);
+        wkt = "POINT(" + event.latlng.lng.toString() + " " + event.latlng.lat.toString() + ")";
+        // console.log(marker, wkt)
+        document.getElementById(inputId).value = wkt;
+    });
+
+}
+
+
+function calculate_elevation_custom(){
+
+    let wkt = document.getElementById('input_wkt').value;
+    let url = 'http://' + window.location.host + '/api/elevation?wkt='+ wkt;
+    
+    let xhr = new XMLHttpRequest();
+    xhr.open("GET", encodeURI(url));
+    xhr.send();
+    xhr.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            $("#output_linestring").html(this.responseText);
+            // console.log(this.responseText);
+        }
+    };
+    
+}
+
+
+function get_wkt(polyline){
+
+    let wkt = "LINESTRING(";
+    latlongs = polyline.getLatLngs();
+
+    latlongs.forEach((element) => {
+        wkt += element.lng + " " + element.lat + ", ";
+    })
+    wkt = wkt.slice(0, -2); 
+    wkt += ')';
+
+    return wkt;
+
+}
+
+
+var polyline = new L.Polyline([]).addTo(map);
+
+function makePolyline(inputId) {
+    let wkt;
+
+    map.off('click');
+    map.on('click', function(event) {
+        clean_map();
+
+        new L.Marker(event.latlng).addTo(map);
+        polyline.addLatLng(event.latlng);
+        wkt = get_wkt(polyline);
+        document.getElementById(inputId).value = wkt;
+
+    });
+
+}
 
